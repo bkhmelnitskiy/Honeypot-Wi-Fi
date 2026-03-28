cf5a9c02762a24652d8234b077ffc5c6be19e120
MiPitera <mpitera@student.agh.edu.pl>
2026-03-28 01:47:53 +0100

    main module, / returns api map

--- webserver/src/app.module.ts
+++ webserver/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller.js';
+import { databaseConfig } from './common/config/database.config.js';
+import { AuthModule } from './auth/auth.module.js';
+import { UsersModule } from './users/users.module.js';
+import { ScansModule } from './scans/scans.module.js';
+import { NetworksModule } from './networks/networks.module.js';
+import { StatsModule } from './stats/stats.module.js';
+import { SyncModule } from './sync/sync.module.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(databaseConfig),
+    ThrottlerModule.forRoot({
+      throttlers: [{ ttl: 60000, limit: 100 }],
+    }),
+    AuthModule,
+    UsersModule,
+    ScansModule,
+    NetworksModule,
+    StatsModule,
+    SyncModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- webserver/src/main.ts
+++ webserver/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module.js';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, transform: true }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN', 'http://localhost:3000'),
+  });
+
+  await app.listen(configService.get<number>('PORT', 3000));
 }
 bootstrap();
