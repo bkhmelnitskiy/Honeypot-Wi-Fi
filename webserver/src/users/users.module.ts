06d780991bc4b9cc6dede20e366b30c6861f4ea9
MiPitera <mpitera@student.agh.edu.pl>
2026-03-28 17:45:32 +0100

    /user API

--- webserver/src/users/users.module.ts
+++ webserver/src/users/users.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Scan } from 'src/scans/entities/scan.entity';
+import { RefreshToken } from 'src/auth/entities/refresh-token.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Scan, RefreshToken])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
